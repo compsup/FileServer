38189345f5cace726839c3ff98b125114a349be0
compsup <duncanmcquat@gmail.com>
2021-01-03 20:34:20 -0500

    Update model.py

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,3 +1,5 @@
+import string
+import random
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from django.db import models
@@ -10,10 +12,19 @@ USERS = (
 )
 
 
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    global rand_string
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+
+
+generate_random_string(16)
+
+
 class Files(models.Model):
     file_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    file = models.FileField(default='')
+    file = models.FileField(default='', upload_to=rand_string)
     public = models.BooleanField(default=False)
     uploaded_by = models.CharField(
         max_length=50, choices=USERS, default='None')
