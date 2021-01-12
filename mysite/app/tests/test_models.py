0991f7296bd187a77679491649c7f288e03dc27e
compsup <duncanmcquat@gmail.com>
2021-01-12 18:31:12 -0500

    Changed tests

--- mysite/app/tests/test_models.py
+++ mysite/app/tests/test_models.py
@@ -5,11 +5,10 @@ from app.models import Files
 class ModelsTestCase(TestCase):
     def test_file_uploading(self):
         file = Files.objects.create(
-            file_name="yolo", pub_date="2020-2-28 2:30", uploaded_by='John', file='awesome_file.txt')
+            file_name="yolo", file='awesome_file.txt')
         file.save()
 
         File = Files.objects.get(file_name="yolo")
 
         self.assertEqual(File.file_name, 'yolo')
-        self.assertEqual(File.uploaded_by, 'John')
         self.assertEqual(File.file, 'awesome_file.txt')
